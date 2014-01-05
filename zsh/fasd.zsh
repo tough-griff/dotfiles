cef9b163f510f0f6dbd9486ac1fef0e282bd9472
Griffin Yourick <gryphon92@gmail.com>
2014-01-05 03:14:15 -0500

    Use fasd

---
+++ zsh/fasd.zsh
@@ -0,0 +1,7 @@
+# Use fasd for ultra-fast navigation
+fasd_cache="$HOME/.fasd-init-zsh"
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+  eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)" >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
