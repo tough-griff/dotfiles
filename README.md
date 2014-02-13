5a984bf39a38d14ded5ee63353da44c0e5111f43
Griffin Yourick <gryphon92@gmail.com>
2014-02-13 16:55:32 -0500

    Work on install script

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-dotfiles
-========
-
+# dotfiles
 My personal configuration and dotfiles.
+
+## installation
+--- WIP ---
+Run the following from your home directory:
+```
+git clone git@github.com:tough-griff/dotfiles.git .dotfiles
+cd .dotfiles
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,9 +1,40 @@
-#!/bin/sh
+#!/bin/zsh
 
 if [ "$TG_DOTFILES_FLAG" == "TRUE" ]; then
   echo "Already installed."
 else
   echo "Installing Tough Griff dotfiles"
-  # cd "$HOME"
-  # TODO
+  cd $HOME
+
+  # Zsh/Prezto installation
+  echo "Installing Prezto and other Zsh configurations"
+  ln -sv .dotfiles/zsh/prezto .zprezto
+
+  setopt EXTENDED_GLOB
+  for rcfile in .zprezto/runcoms/^README.md(.N); do
+    ln -sv "$rcfile" ".${rcfile:t}"
+  done
+
+  # Vim
+  echo "Installing Vim configurations"
+  ln -sv .dotfiles/vim .vim
+  ln -sv .dotfiles/vim/vimrc .vim
+
+  # Git
+  echo "Installing Git dotfiles"
+  ln -sv .dotfiles/git/gitconfig .gitconfig
+  ln -sv .dotfiles/git/gitconfig.user .gitconfig.user
+  ln -sv .dotfiles/git/gitignore .gitignore
+
+  # Ruby
+  echo "Installing Ruby dotfiles"
+  ln -sv .dotfiles/ruby/aprc .aprc
+  ln -sv .dotfiles/ruby/gemrc .gemrc
+  ln -sv .dotfiles/ruby/pryrc .pryrc
+
+  # .config
+  echo "Installing base16-shell config files"
+  mkdir -v .config
+  cd .config
+  ln -sv ../.dotfiles/config/base16-shell
 fi
