e3bae1984eb23c2bf75b071b520ab537d103c6eb
Griffin Yourick <gryphon92@gmail.com>
2014-05-13 14:54:39 -0400

    I don't use vim primarily, so lighten it up a bit

--- install.sh
+++ install.sh
@@ -31,6 +31,7 @@ else
   ln -sv $HOME/.dotfiles/ruby/aprc $HOME/.aprc
   ln -sv $HOME/.dotfiles/ruby/gemrc $HOME/.gemrc
   ln -sv $HOME/.dotfiles/ruby/pryrc $HOME/.pryrc
+  ln -sv $HOME/.dotfiles/ruby/rdebugrc $HOME/.rdebugrc
 
   # .config
   echo "Installing base16-shell config files"
