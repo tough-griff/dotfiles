d047353d2496e4c8267963cf1659754ce0d77675
Griffin Yourick <gryphon92@gmail.com>
2014-08-06 14:01:01 -0400

    prepare for new computer?

--- install.zsh
+++ install.zsh
@@ -1,13 +1,15 @@
 #!/usr/local/bin/zsh
 
-if [ "$TG_DOTFILES_FLAG" = "TRUE" ]; then
+if [[ "$TG_DOTFILES_FLAG" = "TRUE" ]]; then
   echo "Already installed. Aborting."
+elif [[ -z $MYDOTDIR ]]; then
+  echo "Must set MYDOTDIR prior to installing"
 else
-  echo "Installing Tough Griff dotfiles"
+  echo "Installing Tough Griff dotfiles from $MYDOTDIR"
 
   # Zsh/Prezto installation
   echo "Installing Prezto and other Zsh configurations"
-  ln -sv $HOME/.dotfiles/zsh/prezto "${ZDOTDIR:-$HOME}/.zprezto"
+  ln -sv $MYDOTDIR/zsh/prezto "${ZDOTDIR:-$HOME}/.zprezto"
 
   setopt EXTENDED_GLOB
   for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
@@ -18,32 +20,36 @@ else
 
   # Git
   echo "Installing Git dotfiles"
-  for rcfile in $HOME/.dotfiles/git/*; do
+  for rcfile in $MYDOTDIR/git/*; do
     ln -sv "$rcfile" "$HOME/.${rcfile:t}"
   done
 
   # Ruby
   echo "Installing Ruby dotfiles"
-  ln -sv $HOME/.dotfiles/ruby/aprc $HOME/.aprc
-  ln -sv $HOME/.dotfiles/ruby/gemrc $HOME/.gemrc
-  ln -sv $HOME/.dotfiles/ruby/pryrc  $HOME/.pryrc
-  ln -sv $HOME/.dotfiles/ruby/rdebugrc  $HOME/.rdebugrc
+  ln -sv $MYDOTDIR/ruby/aprc $HOME/.aprc
+  ln -sv $MYDOTDIR/ruby/gemrc $HOME/.gemrc
+  ln -sv $MYDOTDIR/ruby/pryrc  $HOME/.pryrc
+  ln -sv $MYDOTDIR/ruby/rdebugrc  $HOME/.rdebugrc
+
+  # .atom
+  echo "Installing Atom config"
+  ln -sv $MYDOTDIR/atom $HOME/.atom
 
   # .config
   echo "Installing base16-shell config files"
   mkdir -v $HOME/.config
-  ln -sv $HOME/.dotfiles/config/base16-shell $HOME/.config/base16-shell
+  ln -sv $MYDOTDIR/config/base16-shell $HOME/.config/base16-shell
 
   # .hushlogin
   echo "Hushing login spam"
-  ln -sv $HOME/.dotfiles/hushlogin $HOME/.hushlogin
+  ln -sv $MYDOTDIR/hushlogin $HOME/.hushlogin
 
   # bin directory
   echo "Installing personal bin directory"
-  ln -sv $HOME/.dotfiles/bin $HOME/bin
+  ln -sv $MYDOTDIR/bin $HOME/bin
 
   # Install Brewfile and Caskfile apps
   export HOMEBREW_CASK_OPTS="--caskroom=/usr/local/Caskroom"
-  brew bundle $HOME/.dotfiles/Brewfile
-  brew bundle $HOME/.dotfiles/Caskfile
+  brew bundle $MYDOTDIR/Brewfile
+  brew bundle $MYDOTDIR/Caskfile
 fi
