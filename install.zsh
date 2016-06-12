e70a5c80d6bf29a78a8458f5c62a347cff7799ad
Griffin Yourick <gryphon92@gmail.com>
2016-06-12 14:31:17 -0400

    Update configs

--- install.zsh
+++ install.zsh
@@ -13,7 +13,7 @@ echo
 echo "git"
 echo "===="
 ln -sfv ${DOTDIR}/git/.gitconfig
-ln -sfv ${DOTDIR}/git/.gitconfig.personal
+[[ -f ${DOTDIR}/.secrets ]] && ln -sfv ${DOTDIR}/git/.gitconfig.personal
 ln -sfv ${DOTDIR}/git/.gitignore
 echo
 
