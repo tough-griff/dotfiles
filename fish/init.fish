48ea32f356538b8384828bc259d0c882d8d2f3d6
Griffin Yourick <gryphon92@gmail.com>
2017-07-06 13:13:35 -0400

    Update configs, add iTerm theme

---
+++ fish/init.fish
@@ -0,0 +1,30 @@
+
+if not type fisher > /dev/null
+  echo "installing fisherman"
+  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+end
+
+set -U fish_color_autosuggestion brblack
+set -U fish_color_cancel red
+set -U fish_color_command green
+set -U fish_color_comment brblack
+set -U fish_color_cwd cyan
+set -U fish_color_end magenta
+set -U fish_color_error red
+set -U fish_color_escape cyan
+set -U fish_color_history_current white
+set -U fish_color_host white
+set -U fish_color_match green
+set -U fish_color_normal white
+set -U fish_color_operator cyan
+set -U fish_color_param white
+set -U fish_color_quote yellow
+set -U fish_color_redirection magenta
+set -U fish_color_search_match --background brblack
+set -U fish_color_selection white
+set -U fish_color_user white
+set -U fish_color_valid_path --underline
+
+set -U fish_pager_color_description yellow
+set -U fish_pager_color_prefix white
+set -U fish_pager_color_progress brblack
