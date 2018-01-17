65218c1e231e5d9de0148e853c3926d63925d30e
Griffin Yourick <gryphon92@gmail.com>
2018-01-17 10:27:34 -0500

    Update configs

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -1,3 +1,16 @@
+// From `hyper-one-dark`
+const black = '#21252b';
+const darkGrey = '#282c34';
+const mediumGrey = '#5c6370';
+const lightGrey = '#abb2bf';
+const white = '#fff';
+const red = '#e06c75';
+const yellow = '#d19a66';
+const green = '#98c379';
+const cyan = '#56b6c2';
+const blue = '#61afef';
+const magenta = '#c678dd';
+
 // Future versions of Hyper may add additional config options,
 // which will not automatically be merged into this file.
 // See https://hyper.is#cfg for all currently supported options.
@@ -14,7 +27,7 @@ module.exports = {
     fontFamily: '"Source Code Pro", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: '#abb2bf',
+    cursorColor: lightGrey,
 
     // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
     cursorShape: 'BLOCK',
@@ -23,52 +36,53 @@ module.exports = {
     cursorBlink: false,
 
     // color of the text
-    foregroundColor: '#abb2bf',
+    foregroundColor: lightGrey,
 
     // terminal background color
-    backgroundColor: '#21252b',
+    backgroundColor: darkGrey,
 
     // border color (window, tabs)
-    borderColor: '#21252b',
+    borderColor: darkGrey,
 
     // custom css to embed in the main window
     css: '',
 
     // custom css to embed in the terminal window
-    termCSS: '',
+    termCSS: `
+      x-screen a {
+        color: inherit;
+      }
 
-    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: '',
-
-    // set to `false` if you want to hide the minimize, maximize and close buttons
-    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
-    showWindowControls: '',
+      x-screen a:hover {
+        color: ${blue};
+      }
+    `,
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '12px 6px',
+    padding: '6px 3px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#21252b',
-      red: '#e06c75',
-      green: '#98c379',
-      yellow: '#d19a66',
-      blue: '#61afef',
-      magenta: '#c678dd',
-      cyan: '#56b6c2',
-      white: '#abb2bf',
-      lightBlack: '#5c6370',
-      lightRed: '#e06c75',
-      lightGreen: '#98c379',
-      lightYellow: '#d19a66',
-      lightBlue: '#61afef',
-      lightMagenta: '#c678dd',
-      lightCyan: '#56b6c2',
-      lightWhite: '#ffffff',
+      black,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      white: lightGrey,
+      lightBlack: mediumGrey,
+      lightRed: red,
+      lightGreen: green,
+      lightYellow: yellow,
+      lightBlue: blue,
+      lightMagenta: magenta,
+      lightCyan: cyan,
+      lightWhite: white,
+      colorCubes: white,
+      grayscale: lightGrey,
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -114,7 +128,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ['hypercwd', 'hyperlinks', 'hyper-statusline'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
