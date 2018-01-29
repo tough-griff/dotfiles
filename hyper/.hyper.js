6693223f3c509f9c7ce95d20a79f26e0d8ffdfba
Griffin Yourick <gryphon92@gmail.com>
2018-01-29 13:48:27 -0500

    Update configs

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -1,4 +1,4 @@
-// From `hyper-one-dark`
+// From my `hyper-one-dark`
 const black = '#21252b';
 const darkGrey = '#282c34';
 const mediumGrey = '#5c6370';
@@ -11,59 +11,22 @@ const cyan = '#56b6c2';
 const blue = '#61afef';
 const magenta = '#c678dd';
 
-// Future versions of Hyper may add additional config options,
-// which will not automatically be merged into this file.
 // See https://hyper.is#cfg for all currently supported options.
 module.exports = {
   config: {
-    // Choose either "stable" for receiving highly polished,
-    // or "canary" for less polished but more frequent updates
     updateChannel: 'stable',
 
-    // default font size in pixels for all tabs
-    fontSize: 12,
-
-    // font family with optional fallbacks
-    fontFamily: '"Source Code Pro", monospace',
-
-    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: lightGrey,
-
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
-    cursorShape: 'BLOCK',
-
-    // set to true for blinking cursor
+    bell: 'SOUND',
+    copyOnSelect: false,
     cursorBlink: false,
+    cursorColor: lightGrey,
+    cursorShape: 'BLOCK',
+    env: {},
+    fontFamily: '"Source Code Pro", monospace',
+    fontSize: 12,
+    shell: '',
+    shellArgs: ['--login'],
 
-    // color of the text
-    foregroundColor: lightGrey,
-
-    // terminal background color
-    backgroundColor: darkGrey,
-
-    // border color (window, tabs)
-    borderColor: darkGrey,
-
-    // custom css to embed in the main window
-    css: '',
-
-    // custom css to embed in the terminal window
-    termCSS: `
-      x-screen a {
-        color: inherit;
-      }
-
-      x-screen a:hover {
-        color: ${blue};
-      }
-    `,
-
-    // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '6px 3px',
-
-    // the full list. if you're going to provide the full color palette,
-    // including the 6 x 6 color cubes and the grayscale map, just provide
-    // an array here instead of a color map object
     colors: {
       black,
       red,
@@ -84,59 +47,41 @@ module.exports = {
       colorCubes: white,
       grayscale: lightGrey,
     },
+    foregroundColor: lightGrey,
+    backgroundColor: darkGrey,
+    borderColor: darkGrey,
+    padding: '6px 3px',
+    css: '',
+    termCSS: `
+      x-screen a {
+        color: inherit;
+      }
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    //
-    // Windows
-    // - Make sure to use a full path if the binary name doesn't work
-    // - Remove `--login` in shellArgs
-    //
-    // Bash on Windows
-    // - Example: `C:\\Windows\\System32\\bash.exe`
-    //
-    // Powershell on Windows
-    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: '',
+      x-screen a:hover {
+        color: ${blue};
+      }
+    `,
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
-    shellArgs: ['--login'],
-
-    // for environment variables
-    env: {},
-
-    // set to false for no bell
-    bell: 'SOUND',
-
-    // if true, selected text will automatically be copied to the clipboard
-    copyOnSelect: false,
-
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
-    // quickEdit: true
-
-    // URL to custom bell
-    // bellSoundURL: 'http://example.com/bell.mp3',
-
-    // for advanced config flags please refer to https://hyper.is/#cfg
+    // plugins...
+    hyperTabs: {
+      border: true,
+      closeAlign: 'right',
+      tabIconsColred: true,
+    },
   },
 
-  // a list of plugins to fetch and install from npm
-  // format: [@org/]project[#version]
-  // examples:
-  //   `hyperpower`
-  //   `@company/project`
-  //   `project#1.0.1`
-  plugins: ['hypercwd', 'hyperlinks', 'hyper-statusline'],
+  plugins: [
+    'hyper-statusline',
+    'hyper-tabs-enhanced',
+    'hypercwd',
+    'hyperlinks',
+  ],
 
-  // in development, you can create a directory under
-  // `~/.hyper_plugins/local/` and include it here
-  // to load it and avoid it being `npm install`ed
-  localPlugins: ['hyper-one-dark'],
+  // `~/.hyper_plugins/local/`
+  localPlugins: [],
 
   keymaps: {
-    // Example
-    // 'window:devtools': 'cmd+alt+o',
+    'tab:next': 'ctrl+tab',
+    'tab:last': 'ctrl+shift+tab',
   },
 };
