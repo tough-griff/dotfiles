787187623e657194acd41af40c88b75a4f16babe
Griffin Yourick <gryphon92@gmail.com>
2021-02-13 15:36:28 -0500

    Improve smartdot

--- fish/functions/_smartdot.fish
+++ fish/functions/_smartdot.fish
@@ -1,14 +1,12 @@
 function _smartdot -d 'Autocompletes "..." to "../.."'
-    set -l cur (commandline -C)
     set -l token (commandline -t)
-    set -l buffer (commandline -b)
+    set -l quoted (string match -r "[\'\"]" -- "$token" ^/dev/null; or echo "")
+    set -l chars (string sub --start -3 -- "$token" ^/dev/null; or echo "")
 
     # If token contains a quoted string, we don't replace...
-    set -l match (string match -r "[\'\"]" -- "$token" ^/dev/null; or echo "")
-    # ...otherwise, if we just typed ..., replace it with ../..
-    set -l chars (string sub --start (math "$cur - 1") --length 2 -- "$buffer" ^/dev/null; or echo "")
-
-    if [ -z "$match" -a "$chars" = ".." ]
+    # ...if the token is simply '..', replace...
+    # ...otherwise, if the token ends with /.., replace
+    if test -z "$quoted" -a \( "$token" = ".." -o "$chars" = "/.." \)
         commandline -i -- "/.."
     else
         commandline -i -- "."
