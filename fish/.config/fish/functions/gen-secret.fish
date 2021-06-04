9b9c7cf77f9f79ae9b98230fdca2b2a1e847f65f
Griffin Yourick <gryphon92@gmail.com>
2021-06-04 14:15:00 -0400

    Daily automated config update

--- fish/.config/fish/functions/gen-secret.fish
+++ fish/.config/fish/functions/gen-secret.fish
@@ -1,3 +1,3 @@
 function gen-secret -a bytes -d "Generate a secret key"
-    node -e "console.log(require('crypto').randomBytes($bytes).toString('base64'));"
+    node -p "require('crypto').randomBytes($bytes).toString('base64');"
 end
