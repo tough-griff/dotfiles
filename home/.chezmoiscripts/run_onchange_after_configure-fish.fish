081e872ddb2d57cb32091750d832bc5de7069a5e
Griffin Yourick <gryphon92@gmail.com>
2022-06-29 14:07:36 -0400

    Keep working on chezmoi

---
+++ home/.chezmoiscripts/run_onchange_after_configure-fish.fish
@@ -0,0 +1,40 @@
+#!/usr/bin/env fish
+set -U fish_color_autosuggestion brblack
+set -U fish_color_cancel red
+set -U fish_color_command green
+set -U fish_color_comment brblack
+set -U fish_color_cwd cyan
+set -U fish_color_end magenta
+set -U fish_color_error red
+set -U fish_color_escape cyan
+set -U fish_color_history_current white
+set -U fish_color_host white
+set -U fish_color_match green
+set -U fish_color_normal white
+set -U fish_color_operator cyan
+set -U fish_color_param white
+set -U fish_color_quote yellow
+set -U fish_color_redirection magenta
+set -U fish_color_search_match --background brblack
+set -U fish_color_selection white
+set -U fish_color_user white
+set -U fish_color_valid_path --underline
+
+set -U fish_pager_color_description yellow
+set -U fish_pager_color_prefix green
+set -U fish_pager_color_progress brblack
+
+# install fisher packages
+curl -sL https://git.io/fisher | source
+fisher install jorgebucaran/fisher
+fisher install franciscolourenco/done
+fisher install jethrokuan/z
+fisher install jorgebucaran/autopair.fish
+fisher install jorgebucaran/replay.fish
+fisher install tough-griff/smartdot.fish
+
+# setup completions
+mkdir -p "$HOME/.config/fish/completions"
+chezmoi completion fish >"$HOME/.config/fish/completions/chezmoi.fish"
+op completion fish >"$HOME/.config/fish/completions/op.fish"
+rustup completions fish >"$HOME/.config/fish/completions/rustup.fish"
--- home/private_dot_config/fish/conf.d/rust.fish
+++ home/private_dot_config/fish/conf.d/rust.fish
@@ -1,10 +1,6 @@
 set -gx RUSTUP_HOME "$HOME/.rustup"
 set -gx CARGO_HOME "$HOME/.cargo"
 
-test -f "$CARGO_HOME/bin/rustup" || exit
+test -x "$CARGO_HOME/bin/rustup" || exit
 
 set -gx PATH "$CARGO_HOME/bin" $PATH
-
-if not test -f "$HOME/.config/fish/completions/rustup.fish"
-    rustup completions fish >"$HOME/.config/fish/completions/rustup.fish"
-end
