507b973c723b91566342c6975e55dd58127fa04b
Griffin Yourick <gryphon92@gmail.com>
2022-06-29 17:22:27 -0400

    README and install script

--- README.md
+++ README.md
@@ -1,50 +1,5 @@
 # Installation
 
-Clone the repo to your desired location, like `~/dotfiles`. The `install.sh`
-script will then install required dependencies, configure your default shell to
-`fish`, and symlink included files to their appropriate locations.
-
 ```sh
-git clone https://github.com/tough-griff/dotfiles.git && cd dotfiles && ./install.sh
-```
-
-Once the install script has completed, start a new terminal session. `fish`, the
-new default shell, should detect that it has not been appropriately initialized
-and configure itself. This time a new terminal session should not be necessary.
-
-To update this repo accordingly you can simply re-run the `install.sh` script.
-
-> NOTE: when adding any new files or functions to your fish config, you will
-> need to re-run the `stow -t "$HOME" fish` command
-
-## macOS Specifics
-
-### Autoupdate
-
-This repository offers the option to configure a `launchd` script to
-automatically commit and push any changes daily.
-
-For this to work, you should fork the repository for yourself and then run the
-following script:
-
-```sh
-./setup-autoupdate-macos.sh
-```
-
-You can view logs for the update script at
-`$HOME/Library/Logs/username.UpdateDotfiles.log`
-
-If at any point you wish to disable the auto-update script, run:
-
-```sh
-launchctl unload -w "$HOME/Library/LaunchAgents/$USER.UpdateDotfiles.plist"
-```
-
-### macOS Defaults
-
-The legendary `~/.macos` script is pulled from https://github.com/mathiasbynens/dotfiles/blob/main/.macos
-with a few tweaks to my liking. Run it with:
-
-```sh
-./macos.sh
+chezmoi init --apply --ssh tough-griff
 ```
--- install.sh
+++ install.sh
@@ -1,102 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-find_brew() {
-  if [[ $(uname -s) == Darwin ]]; then
-    if [[ -x /opt/homebrew/bin/brew ]]; then
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-    elif [[ -x /usr/local/bin/brew ]]; then
-      eval "$(/usr/local/bin/brew shellenv)"
-    fi
+set -euf
+
+if [ ! "$(command -v chezmoi)" ]; then
+  if [ "$(command -v curl)" ]; then
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$HOME/bin"
+  elif [ "$(command -v wget)" ]; then
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$HOME/bin"
   else
-    if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
-      eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    elif [[ -x "$HOME/.linuxbrew/bin/brew" ]]; then
-      eval "$("$HOME/.linuxbrew/bin/brew" shellenv)"
-    fi
+    echo "To install chezmoi, you must have curl or wget installed." >&2
+    exit 1
   fi
 
-  if [[ -n "$HOMEBREW_PREFIX" && -x "$HOMEBREW_PREFIX/bin/brew" ]]; then
-    echo "Using Homebrew at $HOMEBREW_PREFIX"
-    return 0
-  else
-    echo "Unable to locate Homebrew"
-    return 1
-  fi
-}
-
-cd "$(dirname "${BASH_SOURCE[0]}")" || exit 1
-
-if [[ -n "$(command -v git)" ]]; then
-  echo "Ensuring repository is up to date..."
-  git pull origin main
-  echo
+  chezmoi="$HOME/bin/chezmoi"
+else
+  chezmoi=chezmoi
 fi
 
-find_brew
-
-if [[ -z "$(command -v brew)" ]]; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo
-
-  find_brew || exit 1
-fi
-
-brew bundle check || brew bundle install --no-lock
-echo
-
-if [[ -z "$(command -v stow)" ]]; then
-  echo "ERROR: Unable to locate stow, aborting installation."
-  exit 1
-fi
-
-# bash
-stow -t "$HOME" -v bash
-
-# fish
-mkdir -p "$HOME/.config/fish/completions" "$HOME/.config/fish/conf.d" "$HOME/.config/fish/functions"
-if [[ -z "$CODESPACES" ]]; then
-  FISH_PATH="$(command -v fish)"
-  if ! grep -F -q "$FISH_PATH" /etc/shells; then
-    echo "Configuring /etc/shells"
-    echo "$FISH_PATH" | sudo tee -a /etc/shells
-    echo
-  fi
-  if [[ "$SHELL" != "$FISH_PATH" ]]; then
-    echo "Configuring default shell: $FISH_PATH"
-    chsh -s "$FISH_PATH"
-    echo
-  fi
-fi
-stow -t "$HOME" -v fish
-
-# git
-mkdir -p "$HOME/.config/git"
-touch git/.gitconfig
-stow -t "$HOME" -v git
-
-# node
-mkdir -p "$HOME/.nodenv"
-stow -t "$HOME" -v node
-
-# ssh
-mkdir -p "$HOME/.ssh" && chmod 700 "$HOME/.ssh"
-if [[ "$(uname -s)" == Darwin ]]; then
-  mkdir -p "$HOME/.1password" && ln -sf "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" "$HOME/.1password/agent.sock"
-fi
-touch ssh/.ssh/config.personal
-stow -t "$HOME" -v ssh
-
-# starship
-stow -t "$HOME" -v starship
-
-# zsh
-stow -t "$HOME" -v zsh
-
-# miscellanous dotfiles
-stow -t "$HOME" -v misc
-
-# create "reverse" links so we can view config directories from this repo
-ln -sf "$HOME/.config" "$HOME/.nodenv" "$HOME/.rbenv" "$HOME/.pyenv" "$HOME/.ssh" .
-
-echo "Installation complete!"
-echo "Exit and start a new terminal session to continue..."
+$chezmoi init --apply --ssh tough-griff
