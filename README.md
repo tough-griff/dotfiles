35051602e50e13e4df9fac3aa195a61b5d1bbb88
Griffin Yourick <gryphon92@gmail.com>
2022-09-27 11:58:59 -0400

    Refactor install, misc updates

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Installation
+# tough-griff/dotfiles
+
+Managed with [`chezmoi`](https://github.com/twpayne/chezmoi).
+
+Install with:
 
 ```sh
 chezmoi init --apply tough-griff
--- install.sh
+++ install.sh
@@ -2,19 +2,22 @@
 
 set -euf
 
+chezmoi="chezmoi"
+
 if [ ! "$(command -v chezmoi)" ]; then
+  BIN_DIR="$HOME/bin"
+
   if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$HOME/bin"
+    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$BIN_DIR"
   elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$HOME/bin"
+    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$BIN_DIR"
   else
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
   fi
 
-  chezmoi="$HOME/bin/chezmoi"
-else
-  chezmoi=chezmoi
+  chezmoi="$BIN_DIR/chezmoi"
 fi
 
-exec $chezmoi init --apply --source="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+CHEZMOI_DIR="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+exec "$chezmoi" init --apply --source="$CHEZMOI_DIR"
