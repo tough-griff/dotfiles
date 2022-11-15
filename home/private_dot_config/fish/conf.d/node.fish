d7d5c0e87a6ccc8c1cb26d013ec09d83ceb08cc4
Griffin Yourick <gryphon92@gmail.com>
2022-11-15 11:37:14 -0500

    use node 18, add 'npm6' alias

--- home/private_dot_config/fish/conf.d/node.fish
+++ home/private_dot_config/fish/conf.d/node.fish
@@ -4,6 +4,7 @@ abbralias ne "nodenv each -v"
 abbralias npmle "nodenv each -v npm list -g --depth=0"
 
 alias rrepl "env NODE_REPL_MODE=strict NODE_OPTIONS=\"--experimental-repl-await\" npx rrepl"
+alias npm6 "npx npm@latest-6"
 
 set -l bun_install "$HOME/.bun"
 if test -d "$bun_install"
