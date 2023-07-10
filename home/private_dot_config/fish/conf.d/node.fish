920457be5127f407e62e39e3b6a7d1a859ed637c
Griffin Yourick <gryphon92@gmail.com>
2023-07-10 14:05:06 -0400

    Add node version shortcuts

--- home/private_dot_config/fish/conf.d/node.fish
+++ home/private_dot_config/fish/conf.d/node.fish
@@ -1,8 +1,5 @@
 abbralias npmi "npm install"
 
-abbralias ne "nodenv each -v"
-abbralias npmle "nodenv each -v npm list -g --depth=0"
-
 alias rrepl "env NODE_REPL_MODE=strict NODE_OPTIONS=\"--experimental-repl-await\" npx rrepl"
 alias npm6 "npx npm@6"
 alias npm8 "npx npm@8"
--- home/private_dot_config/fish/conf.d/nodenv.fish
+++ home/private_dot_config/fish/conf.d/nodenv.fish
@@ -3,3 +3,10 @@ command -sq nodenv || exit
 status is-interactive && nodenv init - fish | source
 
 set -gx NODE_BUILD_DEFINITIONS "$(brew --prefix node-build-update-defs)/share/node-build:$(brew --prefix node-build)/share/node-build"
+
+abbralias ne "nodenv each -v"
+abbralias npmle "nodenv each -v npm list -g --depth=0"
+
+for v in (nodenv versions --bare)
+    alias "node$v" "env NODENV_VERSION=$v node"
+end
