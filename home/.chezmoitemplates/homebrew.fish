b6de61e1a969f4cf45f3913171a8720b5817343b
Griffin Yourick <gryphon92@gmail.com>
2023-09-18 19:21:38 -0400

    improve syntax highlighting

--- home/.chezmoitemplates/homebrew.fish
+++ home/.chezmoitemplates/homebrew.fish
@@ -1,13 +1,14 @@
-{{- if eq .chezmoi.os "darwin" -}}
+# chezmoi:template:left-delimiter="# {{" right-delimiter="}}"
+# {{- if eq .chezmoi.os "darwin" -}}
 if test -x /opt/homebrew/bin/brew
     eval "$(/opt/homebrew/bin/brew shellenv)"
 else if test -x /usr/local/bin/brew
     eval "$(/usr/local/bin/brew shellenv)"
 end
-{{- else if eq .chezmoi.os "linux" -}}
+# {{- else if eq .chezmoi.os "linux" -}}
 if test -x "/home/linuxbrew/.linuxbrew/bin/brew"
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 else if test -x "$HOME/.linuxbrew/bin/brew"
     eval "$($HOME/.linuxbrew/bin/brew shellenv)"
 end
-{{- end -}}
+# {{- end -}}
