6bd25a01f166f1a7cea59d8c01abf775f689a5ba
Griffin Yourick <gryphon92@gmail.com>
2024-01-18 13:12:21 -0500

    Homebrew-related updates

--- home/private_dot_config/fish/conf.d/exa.fish
+++ home/private_dot_config/fish/conf.d/exa.fish
@@ -1,6 +1,6 @@
-command -sq exa || exit
+command -sq eza || exit
 
-alias ls "exa -F --group-directories-first"
+alias ls "eza -F --group-directories-first"
 abbralias ll "ls -l"
 abbralias la "ls -al"
 abbralias lt "ls -al --tree"
