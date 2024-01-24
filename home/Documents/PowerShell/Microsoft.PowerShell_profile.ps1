035ee7c5b9ebf8e8feaba6b031e68296715b8717
Griffin Yourick <gryphon92@gmail.com>
2024-01-24 15:56:16 -0500

    a few windows related fixes

---
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,14 @@
+Import-Module PSReadLine
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+Invoke-Expression (&starship init powershell)
+
+Import-Module "C:\Program Files\PowerToys\WinUI3Apps\..\WinGetCommandNotFound.psd1"
+
+New-Alias -Name chez -Value "chezmoi"
+
+function edit {
+    code --profile PC $args
+}
