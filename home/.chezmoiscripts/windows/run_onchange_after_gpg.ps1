a5fe82ab9c8adb64b75a1578bf4f105e5d136fff
Griffin Yourick <gryphon92@gmail.com>
2025-03-13 15:23:47 -0400

    some windows scripts

---
+++ home/.chezmoiscripts/windows/run_onchange_after_gpg.ps1
@@ -0,0 +1,4 @@
+# To run this script:
+# 1. Run `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`
+# 2. Run `chezmoi apply ~/.chezmoiscripts/windows/remove-apps.ps1`
+op document get qjak7e3w77zhlohdjedl3ag4vi | gpg --import
---
+++ home/.chezmoiscripts/windows/run_onchange_remove-apps.ps1
@@ -0,0 +1,21 @@
+# To run this script:
+# 1. Run Windows PowerShell as Administrator
+# 2. Run `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`
+# 3. Run `chezmoi apply ~/.chezmoiscripts/windows/remove-apps.ps1`
+
+$Junk = "549981C3F5F10|Clipchamp|Copilot|Bing|GetHelp|NarratorScript|MicrosoftOfficeHub|MicrosoftSolitaireCollection|MicrosoftStickyNotes|OneDrive|Todos|DevHome|People|Photos|WindowsAlarms|WindowsCalculator|WindowsCamera|WindowsCommunicationsApps|WindowsFeedbackHub|WindowsMaps|YourPhone|Zune|MSTeams|QuickAssist"
+
+Get-AppxPackage | Where-Object { $_.Name -match $Junk -and $_.NonRemovable -eq $false } | ForEach-Object {
+    "Removing {0}" -f $_.Name
+    Remove-AppxPackage $_
+}
+
+Get-AppxPackage -AllUsers | Where-Object { $_.Name -match $Junk -and $_.NonRemovable -eq $false } | ForEach-Object {
+    "Removing {0} for all users" -f $_.Name
+    Remove-AppxPackage -AllUsers $_
+}
+
+Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -match $Junk } | ForEach-Object {
+    "Removing provisioned app {0}" -f $_.DisplayName
+    Remove-AppxProvisionedPackage -Online -AllUsers $_
+}
