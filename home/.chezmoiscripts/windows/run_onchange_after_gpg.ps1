0ebba42c9ab012e1d576d68729e5052049a1c802
Griffin Yourick <gryphon92@gmail.com>
2025-03-13 15:49:14 -0400

    gpg management

--- home/.chezmoiscripts/windows/run_onchange_after_gpg.ps1
+++ home/.chezmoiscripts/windows/run_onchange_after_gpg.ps1
@@ -2,3 +2,4 @@
 # 1. Run `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`
 # 2. Run `chezmoi apply ~/.chezmoiscripts/windows/remove-apps.ps1`
 op document get qjak7e3w77zhlohdjedl3ag4vi | gpg --import
+op document get xyofootrl4b2urkh63vpsdztii | gpg --import-ownertrust
