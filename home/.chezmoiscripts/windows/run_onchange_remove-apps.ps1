fa9d196abfdbdf4ff8a8aa75650c9967cefc639f
Griffin Yourick <gryphon92@gmail.com>
2025-03-14 15:20:17 -0400

    windows sucks

--- home/.chezmoiscripts/windows/run_onchange_remove-apps.ps1
+++ home/.chezmoiscripts/windows/run_onchange_remove-apps.ps1
@@ -3,7 +3,7 @@
 # 2. Run `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`
 # 3. Run `chezmoi apply ~/.chezmoiscripts/windows/remove-apps.ps1`
 
-$Junk = "549981C3F5F10|Clipchamp|Copilot|Bing|GetHelp|NarratorScript|MicrosoftOfficeHub|MicrosoftSolitaireCollection|MicrosoftStickyNotes|OneDrive|Todos|DevHome|People|Photos|WindowsAlarms|WindowsCalculator|WindowsCamera|WindowsCommunicationsApps|WindowsFeedbackHub|WindowsMaps|YourPhone|Zune|MSTeams|QuickAssist"
+$Junk = "549981C3F5F10|Clipchamp|Copilot|Bing|GetHelp|NarratorScript|MicrosoftOfficeHub|MicrosoftSolitaireCollection|MicrosoftStickyNotes|OneDrive|Todos|DevHome|People|Photos|WindowsAlarms|WindowsCalculator|WindowsCamera|WindowsCommunicationsApps|WindowsFeedbackHub|WindowsMaps|YourPhone|Zune|MSTeams|QuickAssist|CrossDevice"
 
 Get-AppxPackage | Where-Object { $_.Name -match $Junk -and $_.NonRemovable -eq $false } | ForEach-Object {
     "Removing {0}" -f $_.Name
