cfa6e9376d1d5e49b065f43b63446934a331ec3c
Griffin Yourick <gryphon92@gmail.com>
2025-06-27 11:44:21 -0400

    linux fixes

--- home/.chezmoiscripts/run_onchange_after_gpg.sh
+++ home/.chezmoiscripts/run_onchange_after_gpg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eufo pipefail
-op document get 3glsglf3o2iqrdbxzoe6hjxxnm | gpg --import
-op document get erkyqjbuzxptdvxwlkbyudkn3e | gpg --import-ownertrust
+op document get 3glsglf3o2iqrdbxzoe6hjxxnm | gpg --batch --import
+op document get erkyqjbuzxptdvxwlkbyudkn3e | gpg --batch --import-ownertrust
