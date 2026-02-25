49d5df7241908f49b4770e467fc1c1288bac5b6c
Griffin Yourick <gryphon92@gmail.com>
2026-02-25 14:28:05 -0500

    gpg

--- home/.chezmoiscripts/run_onchange_after_gpg.sh
+++ home/.chezmoiscripts/run_onchange_after_gpg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eufo pipefail
-op document get 3glsglf3o2iqrdbxzoe6hjxxnm | gpg --batch --import
+op document get keut7m33ci3kiovpx3kjjydb4e | gpg --batch --import
 op document get erkyqjbuzxptdvxwlkbyudkn3e | gpg --batch --import-ownertrust
