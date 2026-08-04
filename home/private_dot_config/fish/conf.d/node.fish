db1c2a6b782b2c22d3ecca04844cc45670695156
Griffin Yourick <gryphon92@gmail.com>
2026-08-04 13:24:01 -0400

    manage bun or deno with brew

--- home/private_dot_config/fish/conf.d/node.fish
+++ home/private_dot_config/fish/conf.d/node.fish
@@ -1,3 +1,5 @@
+command -sq npm || exit
+
 abbralias npmi "npm install"
 abbralias npml "npm ls"
 
@@ -5,16 +7,4 @@ alias rrepl "env NODE_REPL_MODE=strict npx rrepl"
 
 for v in (seq 6 10)
     alias "npm$v" "corepack npm@$v"
-end
-
-set -l bun_install "$HOME/.bun"
-if test -d "$bun_install"
-    set -gx BUN_INSTALL $bun_install
-    fish_add_path -gP "$BUN_INSTALL/bin"
-end
-
-set -l deno_install "$HOME/.deno"
-if test -d "$deno_install"
-    set -gx DENO_INSTALL $deno_install
-    fish_add_path -gP "$DENO_INSTALL/bin"
 end
