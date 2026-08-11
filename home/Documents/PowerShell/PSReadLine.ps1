0a12cd236ac5a29df8672ef3ccca77478904a813
Griffin Yourick <gryphon92@gmail.com>
2026-08-11 14:09:17 -0400

    Powershell tweaks

--- home/Documents/PowerShell/PSReadLine.ps1
+++ home/Documents/PowerShell/PSReadLine.ps1
@@ -2,3 +2,6 @@ Import-Module PSReadLine
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadLineKeyHandler -Key Ctrl+D -Function DeleteCharOrExit
