a8c85dc1f8a3628806fe9bbe49c970673404538b
Griffin Yourick <gryphon92@gmail.com>
2026-09-17 09:03:41 -0400

    Borrow zoxide bugfix from https://github.com/ajeetdsouza/zoxide/pull/1260

--- home/private_dot_config/bash/conf.d/zoxide.sh
+++ home/private_dot_config/bash/conf.d/zoxide.sh
@@ -1,3 +1,11 @@
 command -v zoxide >/dev/null 2>&1 || return
 
 eval "$(zoxide init bash)"
+
+# zoxide 0.10.0's generated __zoxide_pwd is missing a command substitution on
+# MSYS/Git Bash, passing the literal string "\builtin pwd -L" to cygpath -w
+# instead of its output. Remove this once upstream ships the fix:
+# https://github.com/ajeetdsouza/zoxide/pull/1260
+function __zoxide_pwd() {
+	\command cygpath -w "$(\builtin pwd -L)"
+}
